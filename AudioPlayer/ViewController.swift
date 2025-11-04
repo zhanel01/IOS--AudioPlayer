27a35f4a54a4fba21060dc97efc4488115de9a46
zhanel01 <zhumanovazhanel@mail.ru>
2025-11-04 20:33:58 +0500

    Initial Commit

---
+++ AudioPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Zhanel  on 04.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AudioPlayerTests/AudioPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerTests.swift
+//  AudioPlayerTests
+//
+//  Created by Zhanel  on 04.11.2025.
+//
+
+import Testing
+@testable import AudioPlayer
+
+struct AudioPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
